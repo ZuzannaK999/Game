0709c2bae3e87b9e1526438452a7552cde97fb79
Zuzanna <100694321+ZuzannaK999@users.noreply.github.com>
2022-06-09 23:09:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -127,3 +127,219 @@ int main(int, char const**)
     }
     music.setLoop(true);
     music.play();
+     //main menu
+    while (MenuWindow.isOpen())
+    {
+        sf::Event event;
+        while (MenuWindow.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                MenuWindow.close();
+            }
+            if(event.type == Event::KeyReleased)
+            {
+                 //choosing a main menu option(using the arrow keys)
+                if(event.key.code == Keyboard::Up)
+                {
+                    MainMenu.MoveUp();
+                    break;
+                }
+                if(event.key.code == Keyboard::Down)
+                {
+                    MainMenu.MoveDown();
+                    break;
+                }
+                if(event.key.code == Keyboard::Return)
+                {
+                    RenderWindow PlayWindow(VideoMode(800,600), "GAME");
+                    Play Play(PlayWindow.getSize().x, PlayWindow.getSize().y);
+                    RenderWindow OptionWindow(VideoMode(800,600), "OPTIONS");
+                    Options Options(OptionWindow.getSize().x, OptionWindow.getSize().y);
+                    int m=MainMenu.MainMenuPressed();
+                    if(m == 0)
+                    {
+                        while(OptionWindow.isOpen())
+                        {
+                            sf::Event OptionEvent;
+                            while(OptionWindow.pollEvent(OptionEvent))
+                            {
+                                if(OptionEvent.type == Event::Closed)
+                                {
+                                    OptionWindow.close();
+                                }
+                                if(OptionEvent.type == Event::KeyReleased)
+                                {
+                                    if(OptionEvent.key.code == Keyboard::Up)
+                                    {
+                                        Options.MoveUp();
+                                    }
+                                    if(OptionEvent.key.code == Keyboard::Down)
+                                    {
+                                        Options.MoveDown();
+                                    }
+                                    if(OptionEvent.key.code == Keyboard::Return)
+                                    {
+                                        int o=Options.OptionPressed();
+                                        //first option: scenery
+                                        if(o == 0)
+                                        {
+                                            RenderWindow SceneryWindow(VideoMode(800,600), "SCENERIES");
+                                            Sceneries Sceneries(SceneryWindow.getSize().x, SceneryWindow.getSize().y);
+                                            while(SceneryWindow.isOpen())
+                                            {
+                                                sf::Event SceneryEvent;
+                                                while(SceneryWindow.pollEvent(SceneryEvent))
+                                                {
+                                                    if(SceneryEvent.type == Event::Closed)
+                                                    {
+                                                        SceneryWindow.close();
+                                                    }
+                                                    if(SceneryEvent.type == Event::KeyReleased)
+                                                    {
+                                                        if(SceneryEvent.key.code == Keyboard::Up)
+                                                        {
+                                                            Sceneries.MoveUpS();
+                                                        }
+                                                        if(SceneryEvent.key.code == Keyboard::Down)
+                                                        {
+                                                            Sceneries.MoveDownS();
+                                                        }
+                                                        if(SceneryEvent.key.code == Keyboard::Return)
+                                                        {
+                                                            int s = Sceneries.SceneryPressed();
+                                                            //first scenery: desert
+                                                            if(s == 0)
+                                                            {
+                                                                background2.setTexture(&desTex);
+                                                            }
+                                                            //second scenery: sea
+                                                            if(s == 1)
+                                                            {
+                                                                background2.setTexture(&islTex);
+                                                            }
+                                                        }
+                                                        if(SceneryEvent.key.code == Keyboard::Escape)
+                                                        {
+                                                            SceneryWindow.close();
+                                                        }
+                                                     }
+                                                }
+                                                SceneryWindow.clear();
+                                                SceneryWindow.draw(background);
+                                                Sceneries.drawS(SceneryWindow);
+                                                SceneryWindow.display();
+                                             }
+                                        }
+                                         if(o == 1)
+                                        {
+                                            //second option: color of the train
+                                          RenderWindow TrainWindow(VideoMode(800,600), "TRAIN");
+                                          Train Train(TrainWindow.getSize().x, TrainWindow.getSize().y);
+                                            while(TrainWindow.isOpen())
+                                            {
+                                                sf::Event TrainEvent;
+                                                while(TrainWindow.pollEvent(TrainEvent))
+                                                {
+                                                    if(TrainEvent.type == Event::Closed)
+                                                    {
+                                                        TrainWindow.close();
+                                                    }
+                                                    if(TrainEvent.type == Event::KeyReleased)
+                                                    {
+                                                        if(TrainEvent.key.code == Keyboard::Up)
+                                                        {
+                                                            Train.MoveUp();
+                                                        }
+                                                        if(TrainEvent.key.code == Keyboard::Down)
+                                                        {
+                                                            Train.MoveDown();
+                                                        }
+                                                        int t=Train.TrainPressed();
+                                                        if(t==1)
+                                                        {
+                                                            front.wagons.setColor(sf::Color::Red);
+                                                        }
+                                                        if(t==2)
+                                                        {
+                                                            front.wagons.setColor(sf::Color::Magenta);
+                                                        }
+                                                        if(TrainEvent.key.code == Keyboard::Escape)
+                                                        {
+                                                            TrainWindow.close();
+                                                        }
+                                                     }
+                                                }
+                                                TrainWindow.clear();
+                                                TrainWindow.draw(background);
+                                                Train.draw(TrainWindow);
+                                                TrainWindow.display();
+                                            }
+                                        }
+                                      //third option: level of difficulty
+                                        if(o == 2)
+                                       {
+                                         RenderWindow LevelWindow(VideoMode(800,600), "LEVEL");
+                                         Level Level(LevelWindow.getSize().x, LevelWindow.getSize().y);
+                                           while(LevelWindow.isOpen())
+                                           {
+                                               sf::Event LevelEvent;
+                                               while(LevelWindow.pollEvent(LevelEvent))
+                                               {
+                                                   if(LevelEvent.type == Event::Closed)
+                                                   {
+                                                       LevelWindow.close();
+                                                   }
+                                                   if(LevelEvent.type == Event::KeyReleased)
+                                                   {
+                                                       if(LevelEvent.key.code == Keyboard::Up)
+                                                       {
+                                                           Level.MoveUp();
+                                                       }
+                                                       if(LevelEvent.key.code == Keyboard::Down)
+                                                       {
+                                                           Level.MoveDown();
+                                                       }
+                                                       if(LevelEvent.key.code == Keyboard::Escape)
+                                                       {
+                                                           LevelWindow.close();
+                                                       }
+                                                    }
+                                                   int i = Level.LevelPressed();
+                                                   if(i==0)
+                                                   {
+                                                       text.setString("EASY");
+                                                       max=40;
+                                                       max_score=3;
+                                                   }
+                                                   if(i==1)
+                                                   {
+                                                       text.setString("MEDIUM");
+                                                       max=25;
+                                                       max_score=4;
+                                                   }
+                                                   if(i==2)
+                                                   {
+                                                       text.setString("HARD");
+                                                       max=15;
+                                                       max_score=5;
+                                                   }
+                                               }
+                                               LevelWindow.clear();
+                                               LevelWindow.draw(background);
+                                               Level.draw(LevelWindow);
+                                               LevelWindow.display();
+                                           }
+                                       }
+                                    }
+                                    if(OptionEvent.key.code == Keyboard::Escape)
+                                    {
+                                        OptionWindow.close();
+                                    }
+                                }
+                              }
+                            OptionWindow.clear();
+                            OptionWindow.draw(background);
+                            Options.draw(OptionWindow);
+                            OptionWindow.display();
+                          }
+                     }
