77e4a4393b50641c3c6d2375640254b4580479c4
Zuzanna <100694321+ZuzannaK999@users.noreply.github.com>
2022-06-10 21:58:20 +0200

    Update Train.cpp

--- Train.cpp
+++ Train.cpp
@@ -14,9 +14,9 @@ Train::Train(float widthT, float heightT)
         text[1].setString("Red");
         text[2].setString("Purple");
 
-    text[0].setPosition(sf::Vector2f(widthT/2, heightT/(max_num_of_choices_T + 1)*1));
-    text[1].setPosition(sf::Vector2f(widthT/2, heightT/(max_num_of_choices_T + 1)*2));
-    text[2].setPosition(sf::Vector2f(widthT/2, heightT/(max_num_of_choices_T + 1)*3));
+    text[0].setPosition(sf::Vector2f(widthT/2, heightT/(max_num_of_choices + 1)*1));
+    text[1].setPosition(sf::Vector2f(widthT/2, heightT/(max_num_of_choices + 1)*2));
+    text[2].setPosition(sf::Vector2f(widthT/2, heightT/(max_num_of_choices + 1)*3));
     
     Selected=0;
 }
