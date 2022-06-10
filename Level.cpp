87f7dd79e5abd0b13f25ce30d27e85307fb8b3b5
Zuzanna <100694321+ZuzannaK999@users.noreply.github.com>
2022-06-10 21:55:35 +0200

    Update Level.cpp

--- Level.cpp
+++ Level.cpp
@@ -9,13 +9,13 @@
 Level::Level(float widthL, float heightL)
 {
     text[0].setString("Easy");
-    text[0].setPosition(sf::Vector2f(widthL/2, heightL/(max_num_of_choices_L + 1)*1));
+    text[0].setPosition(sf::Vector2f(widthL/2, heightL/(max_num_of_choices + 1)*1));
     
     text[1].setString("Medium");
-    text[1].setPosition(sf::Vector2f(widthL/2, heightL/(max_num_of_choices_L + 1)*2));
+    text[1].setPosition(sf::Vector2f(widthL/2, heightL/(max_num_of_choices + 1)*2));
     
     text[2].setString("Hard");
-    text[2].setPosition(sf::Vector2f(widthL/2, heightL/(max_num_of_choices_L + 1)*3));
+    text[2].setPosition(sf::Vector2f(widthL/2, heightL/(max_num_of_choices + 1)*3));
     
     Selected=0;
 }
