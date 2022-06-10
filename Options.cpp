6897e5e82b1c82dae12f5603f5ae7f8945b2c0a0
Zuzanna <100694321+ZuzannaK999@users.noreply.github.com>
2022-06-10 22:01:32 +0200

    Update Options.cpp

--- Options.cpp
+++ Options.cpp
@@ -11,13 +11,13 @@
 Options::Options(float widthO, float heightO)
 {
     text[0].setString("Sceneries");
-    text[0].setPosition(sf::Vector2f(widthO/2, heightO/(max_num_of_choices_O + 1)*1));
+    text[0].setPosition(sf::Vector2f(widthO/2, heightO/(max_num_of_choices + 1)*1));
    
     text[1].setString("Train");
-    text[1].setPosition(sf::Vector2f(widthO/2, heightO/(max_num_of_choices_O + 1)*2));
+    text[1].setPosition(sf::Vector2f(widthO/2, heightO/(max_num_of_choices + 1)*2));
     
     text[2].setString("Level");
-    text[2].setPosition(sf::Vector2f(widthO/2, heightO/(max_num_of_choices_O + 1)*3));
+    text[2].setPosition(sf::Vector2f(widthO/2, heightO/(max_num_of_choices + 1)*3));
     
     Selected=0;
 }
